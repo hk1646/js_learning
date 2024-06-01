a1dacff3afa72cb94420b6e45324979d5f4a649e
hk1646 <hk1646455@gmail.com>
2024-06-01 10:08:34 +0530

    initial commit

---
+++ greaterOfThree.js
@@ -0,0 +1,12 @@
+let x = 0; 
+let y = 1;
+let z = 2;
+if (x > y && x > z){
+    console.log(x,"is a Big Number")
+}
+else if(y > z && y > x){
+    console.log(y,"is a Big Number")
+}
+else{
+     console.log(z,"is a Big Number")
+}
---
+++ loops.js
@@ -0,0 +1,73 @@
+// let i = 100;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+    
+// }
+
+// let i = 60;
+//  while(i <= 90) {
+//     console.log(String.fromCharCode(i));
+//     i++;
+// }
+// let sum = 0;
+// for(i=1;i<=1000;i++){
+//         sum = sum + i;    
+  
+// }
+// console.log("Total sum of",sum)
+
+// function swapping(n1,n2){
+//     let p = n1;
+//      n1 = n2;
+//      n2 = p; 
+//     console.log( "After swapping :", n1,n2);
+// }
+// swapping(2,4)
+
+
+// function check(Tam,Eng,Mat,Sci,Soc){
+//     if(num1 % 2 != 0){
+//           console.log("The entered number is odd. ");
+//     }else{
+//         console.log("The entered number is even. ");
+//     }
+//     }
+
+// check()
+
+// function add(num1)
+
+// function check(Tam,Eng,Mat,Sci,Soc){
+//     let Total = Tam+Eng+Mat+Sci+Soc;
+//     console.log("The Total mark: ", Total);
+//     let Avg = Total/5.0;
+//     console.log("The Average score is :", Avg)
+//     let Per = (Total/500.0)*100;
+//     console.log("The percentage score is :", Per)
+// }
+// check(77,100,100,100,100);
+
+
+// let Total = [23,34,42,67,45]
+// friend[1] = "Rohit";
+// console.log();
+
+// let friend = new (class {
+//     getFriName(){
+//         console.log("Anshu")
+//     }
+// })();
+// friend.getFriName();
+
+
+let friend = ['anshu', 'tek','hari'];
+friend.splice(1,2)
+console.log(friend)
+
+let order = [
+    {no:1,item:"mobile"},
+    {no:2,item:"watch"},
+    {no:3,item:"shoe"}
+]
+console.log(order.find{})
